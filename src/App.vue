92de3b08ae4b70caffd045ce30bcc7e404bf24f3
Samson Moses <knomic10@gmail.com>
2023-06-01 04:48:52 +0100

    installed tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
     <router-link to="/post-a-job">Post A Job</router-link> |
   </nav>
   <router-view/>
