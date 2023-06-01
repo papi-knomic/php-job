60aba87002669a3fe45af4fed3e433359e7880f9
Samson Moses <knomic10@gmail.com>
2023-06-01 04:03:57 +0100

    init

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import NotFoundView from '../views/404.vue'
 
 const routes = [
   {
@@ -14,6 +15,13 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  
+  //catch all 404
+  {
+    path: '/:catchAll(.*)',
+    name: '404',
+    component: NotFoundView
   }
 ]
 
